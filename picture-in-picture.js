5eff55b1d6a7aada8e65ee3131230d1be7fbabe3
Bruno Macedo <contato@brunomacedo.com.br>
2018-11-05 13:12:20 -0200

    [JS] extension code

---
+++ picture-in-picture.js
@@ -0,0 +1,58 @@
+/**
+ * Picture-in-Picture
+ * Editor’s Draft, 30 October 2018
+ * https://wicg.github.io/picture-in-picture/
+ * @param pictureInPictureEnabled
+ * Repo: https://github.com/brunomacedo/video-picture-in-picture
+ */
+const video = document.querySelector('.video-stream.html5-main-video')
+const container = document.querySelector('#container.style-scope.ytd-player')
+
+const buttonB = document.createElement('button')
+  buttonB.className = 'yt-float-button'
+  buttonB.innerText = 'Float'
+  container.append(buttonB)
+
+const togglePipButton = document.querySelector('.yt-float-button')
+const styleFloatButton = document.createElement('style')
+  styleFloatButton.innerText = `
+  #container.style-scope.ytd-player:hover .yt-float-button {
+    display: block;
+  }
+  .yt-float-button {
+    display: none;
+    position: absolute;
+    left: 50%;
+    top: 10px;
+    z-index: 850;
+    transform: translateX(-50%);
+    text-indent: -9999px;
+    font-size: 0;
+    white-space: nowrap;
+    overflow: visible;
+    width: 40px;
+    height: 40px;
+    border: 0;
+    background-color: transparent;
+    background-image: url(https://i.imgur.com/3KUHz3g.png);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+  }`
+
+  typeof container !== 'undefined' ? container.append(styleFloatButton) : console.log('null')
+
+togglePipButton.hidden = !document.pictureInPictureEnabled || video.disablePictureInPicture
+  togglePipButton.addEventListener('click', function() {
+    if (!document.pictureInPictureElement) {
+      video.requestPictureInPicture()
+      .catch(error => {
+        // Video failed to enter Picture-in-Picture mode.
+      })
+    } else {
+      document.exitPictureInPicture()
+      .catch(error => {
+        // Video failed to leave Picture-in-Picture mode.
+      })
+    }
+  })
